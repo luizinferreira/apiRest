4c5216fc3077c2b94371e0cad148ebf2cc2813af
Luiz Eduardo <luizeduardo_2904@live.com>
2020-12-16 16:27:54 -0400

    Repository Initial

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+ module.exports =  {
+     dialect: 'mssql',
+     host: '192.168.0.104',
+     username: 'SQLAPI',
+     password: 'SQLAPI',
+     database: 'protheusteste',
+     define: {
+         timestamps: true,
+         underscored: true,
+     },
+ };
